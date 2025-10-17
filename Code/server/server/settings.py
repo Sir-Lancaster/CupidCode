698dcf446d4dffd0f30eb72f11ce3acba02ca4ad
Kayden <krazin0202@gmail.com>
2025-10-17 16:58:00 -0600

    Commented out in .yml where it was redefining debug as false in settings.py. Changed views.py and vite.config.js to point to the same directory for the manifest. changed settings.py to parse the .env file for debug settings and allowed hosts.

--- Code/server/server/settings.py
+++ Code/server/server/settings.py
@@ -26,14 +26,9 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-x_e(m+z0-supl3g7n5zi1es0zwecn#_ipb920xxec)%v6^ah5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'https://cupidcode.zapto.org/'
-]
-
+ALLOWED_HOSTS = [h for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h]
 VITE_APP_DIR = BASE_DIR / "src"
 
 
