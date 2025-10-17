e40c4c7ecd76a100abbbc913072b3d7561f3a7ca
Nova xyzv <phoenixyzv@gmail.com>
2025-10-17 16:48:39 -0600

    restore commit 8b6fe615 #2

--- Code/server/server/settings.py
+++ Code/server/server/settings.py
@@ -26,9 +26,13 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-x_e(m+z0-supl3g7n5zi1es0zwecn#_ipb920xxec)%v6^ah5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = [h for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://cupidcode.zapto.org/'
+]
 
 VITE_APP_DIR = BASE_DIR / "src"
 
